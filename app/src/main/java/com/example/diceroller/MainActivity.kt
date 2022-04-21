6cc96f840a0dc467d92bd12f4836b6f25843713b
FreeER <2312577+FreeER@users.noreply.github.com>
2022-04-21 03:22:05 +0000

    images added for 6 sided dice thanks to https://developer.android.com/codelabs/kotlin-android-training-images-compat#2 / https://github.com/google-developer-training/android-kotlin-fundamentals-apps/tree/master/DiceRoller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.diceroller
 
+import android.R.drawable
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.reflect.Field
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +27,47 @@ class MainActivity : AppCompatActivity() {
         neededTicks = 3
         val but = findViewById<Button>(R.id.roll_button)
         but.isEnabled = false
-        val sides = findViewById<RadioButton>(findViewById<RadioGroup>(R.id.diceGroup).checkedRadioButtonId).text.toString().toInt()
+        val sides = try {
+            findViewById<RadioButton>(findViewById<RadioGroup>(R.id.diceGroup).checkedRadioButtonId).text.toString().toInt()
+        } catch (e: Exception) { 6 }
         val vantage = findViewById<CheckBox>(R.id.advantage).isChecked
+        val dice1 = findViewById<ImageView>(R.id.diceImage1)
+        val dice2 = findViewById<ImageView>(R.id.diceImage2)
         object : CountDownTimer(3000, 30) {
             override fun onTick(secondsUntilDone: Long) {
                 timerTicks++
                 if (timerTicks > neededTicks)
                 {
                     neededTicks = (neededTicks * 1.4).toInt()
-                    (1..sides).random().toString()
-                    resultText.text =
-                        if(!vantage) (1..sides).random().toString()
-                        else (1..sides).random().toString() + "    " + (1..sides).random().toString()
+                    val a = (1..sides).random().toString()
+                    val b = (1..sides).random().toString()
+                    if(sides == 6)
+                    {
+                        // https://stackoverflow.com/questions/4427608/android-getting-resource-id-from-string
+                            try {
+                                val resIDa = getResId("dice_${a}", R.drawable::class.java)
+                                val resIDb = getResId("dice_${b}", R.drawable::class.java)
+                                resultText.text = ""
+                                dice1.setImageResource(resIDa)
+                                dice2.setImageResource(if (vantage) resIDb else R.drawable.empty_dice)
+                            } catch (e: Exception) {
+                                resultText.text = if(!vantage) a else "$a    $b"
+                            }
+                    } else {
+                        resultText.text = if(!vantage) a else "$a    $b"
+                        dice1.setImageResource(R.drawable.empty_dice)
+                        dice2.setImageResource(R.drawable.empty_dice)
+                    }
                 }
             }
             override fun onFinish() {
                 but.isEnabled = true
             }
         }.start()
+    }
+
+    fun getResId(resName: String, c: Class<*>): Int {
+        val idField: Field = c.getDeclaredField(resName)
+        return idField.getInt(idField)
     }
 }
